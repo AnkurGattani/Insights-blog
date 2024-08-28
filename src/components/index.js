66cc5ec40d285ee8fd608e3800053bdcf67bf04a
Ankur Gattani <ankurgattani02@gmail.com>
2024-08-28 07:55:47 +0530

    Add index.js file to export all components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Button from "./Miscellaneous/Button"
+import Input from "./Miscellaneous/Input";
+import PostCard from "./Miscellaneous/PostCard";
+import Login from "./Authentication/Login";
+import SignUp from "./Authentication/SignUp";
+import RTE from "./Post Form/RTE";
+import PostForm from "./Post Form/PostForm";
+
+export {
+	Header,
+	Footer,
+	Button,
+	Input,
+	PostCard,
+	Login,
+	SignUp,
+	RTE,
+	PostForm
+};
