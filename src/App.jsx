f4f4b5d6cc767b0e71725e65571908828ad3aa27
Ankur Gattani <ankurgattani02@gmail.com>
2024-09-06 18:30:13 +0530

    Update App component to include Header, Footer, and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import { useDispatch } from 'react-redux';
 import authService from './services/auth';
 import { login, logout } from './store/slices/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -10,34 +13,31 @@ function App() {
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then((userData) => {
-      if(userData) {
-        dispatch(login(userData));
-      }
-      else {  // if user is not logged in, logout to make sure state is always updated
-        dispatch(logout());
-      }
-    })
-    .catch((error) => {
-      console.log('Error in App :: useEffect : ', error);
-    })
-    .finally( () => setLoading(false))
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {  // if user is not logged in, logout to make sure state is always updated
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log('Error in App :: useEffect : ', error);
+      })
+      .finally(() => setLoading(false))
   }, []);
 
-  if(loading) {
-    return (
-      <>
-        <h1>Loading...</h1>
-      </>
-    )
-  }
-  else {
-    return (
-      <>
-        <h1>Welcome to Insights!</h1>
-      </>
-    )
-  }
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap '>
+      <div className='w-full flex flex-col justify-between' >
+        <Header />
+        <main className='mt-[8vh]'>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
