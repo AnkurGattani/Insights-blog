9b51675f1f69c2cd5e45b5dd77f6fb1be7aef35e
Ankur Gattani <ankurgattani02@gmail.com>
2024-09-06 18:25:27 +0530

    Update Footer component with responsive design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-	<footer className='w-full h-[6vh] text-black flex items-center justify-center '>
-    <p>
-      &copy; 2024. All rights reserved. Made with ❤️ by {' '}
-    </p>
-    <Link 
-      to="https://www.linkedin.com/in/ankurgattani/"
-      className='font-bold'
-      target='_blank'
-    >
-      Ankur Gattani
-    </Link>
-  </footer>
+    <footer className='w-full h-[6vh] text-black flex items-center md:text-lg text-sm text-center justify-center '>
+      <p className='flex items-center justify-around'>
+        &copy; 2024. All rights reserved. Made with ❤️ by &nbsp;
+        <Link
+          to="https://www.linkedin.com/in/ankurgattani/"
+          className='font-bold'
+          target='_blank'
+        >
+          Ankur Gattani
+        </Link>
+      </p>
+    </footer>
   );
 }
 
